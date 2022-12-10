4ec328051cc6b2fa0346382636084f0e074aa95c
gmbt18 <84000377+gmbt18@users.noreply.github.com>
2022-12-10 13:29:22 +0800

    added comments for the views.py & models.py files

--- hregistry/accounts/views.py
+++ hregistry/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Create function for user registration
+
+# Create function for user signup
+
+# Create function for user edit account
+
+# Create function for user remove account
--- hregistry/patients/views.py
+++ hregistry/patients/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Create function for adding patient
+
+# Create function for removing patient
+
+# Create function for editing patient details
+
+# Create function for patient search
+
+# Create function for patient filter
+
+# Create function for pdf export

0fcfa25747b4c390beda9e41d34ac00e0f888f12
Cedric Caquilala <cmcaquilala@up.edu.ph>
2022-12-11 23:44:25 +0800

    added user accounts, combined all person classes into Staff class, linked both

--- hregistry/accounts/models.py
+++ hregistry/accounts/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 # Enter user models here
+class AuthUser(AbstractUser):
+    USER_TYPE_CHOICES = {
+        (1, 'Nurse'),
+        (2, 'Doctor'),
+        (3, 'Medtech'),
+        (4, 'Radtech'),
+    }
+
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES,null=True)
+
--- hregistry/patients/admin.py
+++ hregistry/patients/admin.py
@@ -3,8 +3,9 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Record)
-admin.site.register(Angiographer)
-admin.site.register(Anesthesiologist)
-admin.site.register(Nurse)
-admin.site.register(Category)
-admin.site.register(Procedure)
+admin.site.register(Staff)
+# admin.site.register(Angiographer)
+# admin.site.register(Anesthesiologist)
+# admin.site.register(Nurse)
+# admin.site.register(Category)
+# admin.site.register(Procedure)
